4ffe675a48aad2083b8f3aacc9edba6d00c286c5
NEKOmatta <nekomatta77@gmail.com>
2026-04-27 20:24:15 +0800

    идеал

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Как только этот файл начал выполняться, МГНОВЕННО убиваем лоадер
-const loader = document.getElementById('initial-loader');
-if (loader) {
-  loader.remove();
-}
-
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
